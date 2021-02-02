51819c43e2d849ef5c62b4c5672a398563f1ab21
hanuilee <31558727+hanuilee@users.noreply.github.com>
2021-02-02 09:55:47 +0900

    Update firstUniqChar.cpp

--- String/firstUniqChar.cpp
+++ String/firstUniqChar.cpp
@@ -18,3 +18,20 @@ public:
         return -1;
     }
 };
+
+
+
+
+//********************//
+int firstUniqChar(string s) {
+    
+        int count[26] = {0};
+        for(int i = 0; i<s.length(); i++)
+            count[s[i] - 'a']++;
+        
+        for(int i = 0; i<s.length(); i++)
+            if(count[s[i] - 'a'] == 1)
+                return i;
+        
+        return -1;
+    }
