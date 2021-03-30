ba8343d1b018eab68ec6d88cf13800283f008172
hanuilee <31558727+hanuilee@users.noreply.github.com>
2021-03-31 08:57:53 +0900

    Add files via upload

---
+++ Dynamic Programming/maxSubArray.cpp
@@ -0,0 +1,18 @@
+// Runtime: 8 ms
+// Memory Usage: 13 MB
+
+using namespace std;
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxSum = nums[0];
+        int curSum = nums[0];
+        for(int i = 1; i < nums.size(); i++)
+        {
+            curSum = max(curSum + nums[i], nums[i]);   
+            maxSum = max(maxSum, curSum);
+        }
+        return maxSum;
+    }
+};
